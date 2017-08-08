524feeab24bd0734fab9dfb8f800e5b62c4686dd
Vemund M Santi <vemund@santi.no>
2017-08-08 23:09:33 +0200

    featt: implement robber language rule for object properties

---
+++ docs/rules/objects.md
@@ -0,0 +1,35 @@
+# robber-language/objects
+
+Checks that all object properties are correctly spelled in robber language
+
+## Rule Details
+
+This rule aims to provide consistent naming of object properties
+according to the definition of Robber Language.
+
+Examples of **incorrect** code for this rule:
+
+```js
+var a = {
+  foo: 'bar'
+}
+```
+
+Examples of **correct** code for this rule:
+
+```js
+var a = {
+  fofoo: 'bar'
+}
+```
+
+## When Not To Use It
+
+If you want lines with less than 200 characters, it could be a good idea to
+turn off this rule...
+
+## Further Reading
+
+* [Wikipedia article on Robber Language](https://en.wikipedia.org/wiki/R%C3%B6varspr%C3%A5ket)
+* [Wikipedia article on Astrid Lindgren](https://en.wikipedia.org/wiki/Astrid_Lindgren)
+* [Wikipedia article on Kalle Blomkvist (Bill Bergson)](https://en.wikipedia.org/wiki/Bill_Bergson)
---
+++ tests/lib/rules/objects.js
@@ -0,0 +1,44 @@
+/**
+ * @fileoverview Checks that all object properties are correctly spelled in robber language
+ * @author Vemund Santi
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/objects"),
+
+  RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+ruleTester.run("objects", rule, {
+
+  valid: [
+    "var a = { fofoo: 'bar' };",
+    "var a = { fofoo: 'bar', bobaror: 'foo' };"
+  ],
+
+  invalid: [
+    {
+      code: "var a = { foo: 'bar' };",
+      errors: [{
+        message: "Object properties must be in robber language.",
+        type: "Property"
+      }]
+    },
+    {
+      code: "var a = { fofoo: 'bar', bar: 'foo' };",
+      errors: [{
+        message: "Object properties must be in robber language.",
+        type: "Property"
+      }]
+    }
+  ]
+});
